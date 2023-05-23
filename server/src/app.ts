3eb4a2ad2006ca775c52f5c6a9391c969bcda492
Vlad <vladyslav.o@enkonix.com>
2023-05-23 18:36:55 +0300

    Delete Public

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,11 @@
 import express from "express";
 import cors from "cors";
 import { planetsRouter } from "./routes/planets/planets.router.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const corsOptions = {
   origin: "http://localhost:3000",
@@ -10,7 +15,10 @@ const app = express();
 
 app.use(cors(corsOptions));
 app.use(express.json());
-
+app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(planetsRouter);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 export { app };
